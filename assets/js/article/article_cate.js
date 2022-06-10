a8fb9ae63756d7d0eb279bbaa0fa5c721ac88eea
lkx <lkx3091705416@126.com>
2022-06-10 09:29:00 +0800

    修改了一些细微的错误

--- assets/js/article/article_cate.js
+++ assets/js/article/article_cate.js
@@ -1,6 +1,6 @@
 $(function () {
-  let layer = layui.layer;
-  let form = layui.form;
+  let layer = layui.layer
+  let form = layui.form
   //获取文章分类列表
   /**
    * 发送ajax请求获取文章类别数据
@@ -17,109 +17,109 @@ $(function () {
    */
   function initArticleCateList() {
     $.ajax({
-      method: "get",
-      url: "/my/article/cates",
+      method: 'get',
+      url: '/my/article/cates',
       success: (res) => {
-        if (res.status !== 0) return layer.msg(res.message);
-        console.log(res);
-        const htmlStr = template("tpl-table", res);
+        if (res.status !== 0) return layer.msg(res.message)
+        console.log(res)
+        const htmlStr = template('tpl-table', res)
         // console.log(111);
-        $("tbody").html(htmlStr);
+        $('tbody').html(htmlStr)
       },
-    });
+    })
   }
-  initArticleCateList();
+  initArticleCateList()
   //为添加按钮绑定点击事件
-  let indexAdd = null;
-  $("#btnAddCate").on("click", () => {
+  let indexAdd = null
+  $('#btnAddCate').on('click', () => {
     indexAdd = layer.open({
       type: 1,
-      area: ["500px", "250px"],
-      title: "添加文章分类",
-      content: $("#addCateBox").html(),
-    });
-  });
+      area: ['500px', '250px'],
+      title: '添加文章分类',
+      content: $('#addCateBox').html(),
+    })
+  })
   //通过代理的形式,为form-add添加submit事件
-  $("body").on("submit", "#form-add", function (e) {
+  $('body').on('submit', '#form-add', (e) => {
     //阻止默认提交行为
-    e.preventDefault();
+    e.preventDefault()
     // console.log('ok');
     $.ajax({
-      method: "post",
-      url: "/my/article/addcates",
+      method: 'post',
+      url: '/my/article/addcates',
       data: $(this).serialize(),
       success: (res) => {
-        if (res.status !== 0) return layer.msg("新增分类失败");
-        initArticleCateList();
-        layer.msg("新增分类成功");
+        if (res.status !== 0) return layer.msg('新增分类失败')
+        initArticleCateList()
+        layer.msg('新增分类成功')
         //根据indexAdd关闭弹窗
-        layer.close(indexAdd);
+        layer.close(indexAdd)
       },
-    });
-  });
+    })
+  })
   //通过代理的方式,为编辑按钮绑定点击事件
-  let indexEdit = null;
-  $("tbody").on("click", ".btn-edit", function () {
+  let indexEdit = null
+  $('tbody').on('click', '.btn-edit', () => {
     //弹出一个修改文章信息分类的层
     indexEdit = layer.open({
       type: 1,
-      area: ["500px", "250px"],
-      title: "修改文章分类",
-      content: $("#dialog-edit").html(),
-    });
-    let id = $(this).attr("data-id");
+      area: ['500px', '250px'],
+      title: '修改文章分类',
+      content: $('#dialog-edit').html(),
+    })
+    let id = $(this).attr('data-id')
     //发起ajax请求,获取对应id的文章分类信息
     $.ajax({
-      method: "get",
-      url: "/my/article/cates/" + id,
+      method: 'get',
+      url: '/my/article/cates/' + id,
       success: (res) => {
-        if (res.status !== 0) return layer.msg("获取文章分类信息失败");
+        if (res.status !== 0) return layer.msg('获取文章分类信息失败')
         // console.log(res);
         //将获取到的文章分类信息,填充到弹窗中
-        form.val("form-edit", res.data);
+        form.val('form-edit', res.data)
       },
-    });
-  });
+    })
+  })
   //通过代理的形式,为form-edit添加submit事件
-  $("body").on("submit", "#form-edit", function (e) {
+  $('body').on('submit', '#form-edit', function (e) {
     //阻止默认提交行为
-    e.preventDefault();
+    e.preventDefault()
     //发起ajax请求,修改文章分类信息
     $.ajax({
-      method: "post",
-      url: "/my/article/updatecate",
+      method: 'post',
+      url: '/my/article/updatecate',
       data: $(this).serialize(),
       success: (res) => {
-        if (res.status !== 0) return layer.msg("修改文章分类失败");
-        layer.msg("修改文章分类成功");
-        initArticleCateList();
+        if (res.status !== 0) return layer.msg('修改文章分类失败')
+        layer.msg('修改文章分类成功')
+        initArticleCateList()
         //根据indexEdit关闭弹窗
-        layer.close(indexEdit);
+        layer.close(indexEdit)
       },
-    });
-  });
+    })
+  })
   //通过代理的形式,为删除按钮绑定点击事件
-  $("tbody").on("click", ".btn-delete", function () {
+  $('tbody').on('click', '.btn-delete', function () {
     //获取当前点击的删除按钮的文章分类id
-    let id = $(this).attr("data-id");
+    let id = $(this).attr('data-id')
     //弹出一个确认框,确认是否删除文章
     layer.confirm(
-      "确认删除该文章分类吗?",
-      { icon: 3, title: "提示" },
+      '确认删除该文章分类吗?',
+      { icon: 3, title: '提示' },
       function (index) {
         //发起ajax请求,删除文章分类
         $.ajax({
-          method: "get",
-          url: "/my/article/deletecate/" + id,
+          method: 'get',
+          url: '/my/article/deletecate/' + id,
           success: (res) => {
             //判断删除是否成功
-            if (res.status !== 0) return layer.msg("删除文章分类失败");
-            layer.msg("删除文章分类成功");
-            initArticleCateList();
-            layer.close(index); //关闭确认框
+            if (res.status !== 0) return layer.msg('删除文章分类失败')
+            layer.msg('删除文章分类成功')
+            initArticleCateList()
+            layer.close(index) //关闭确认框
           },
-        });
+        })
       }
-    );
-  });
-});
+    )
+  })
+})
--- assets/js/article/article_edit.js
+++ assets/js/article/article_edit.js
@@ -1,29 +1,29 @@
 $(function () {
   //初始化渲染页面,将文章数据渲染到页面
-  let data = window.location.search;
-  let Id = data.slice(4);
-  console.log(Id);
-  initArticle(Id);
+  let data = window.location.search
+  let Id = data.slice(4)
+  console.log(Id)
+  initArticle(Id)
   function initArticle(ID) {
     //发起ajax请求,获取当前文章的详细信息
     $.ajax({
-      method: "GET",
-      url: "/my/article/" + ID,
+      method: 'GET',
+      url: '/my/article/' + ID,
       success: (res) => {
         // console.log(res);
-        if (res.status !== 0) return layer.msg("获取文章详情失败");
+        if (res.status !== 0) return layer.msg('获取文章详情失败')
         //调用模板引擎的render方法,渲染编辑文章的表单
         //为编辑区域的表单快速赋值
-        form.val("form-edit", res.data);
+        form.val('form-edit', res.data)
       },
-    });
+    })
   }
   //导入layer和form对象
-  var layer = layui.layer;
-  var form = layui.form;
-  initCate();
+  var layer = layui.layer
+  var form = layui.form
+  initCate()
   // 初始化富文本编辑器
-  initEditor();
+  initEditor()
   /**
    * 定义渲染文章分类的方法
    * 1.封装函数,调用方法
@@ -34,49 +34,49 @@ $(function () {
   //定义加载文章分类的函数
   function initCate() {
     $.ajax({
-      url: "/my/article/cates",
-      type: "get",
-      success: function (res) {
+      url: '/my/article/cates',
+      type: 'get',
+      success: (res) => {
         // console.log(res.data);
-        if (res.status !== 0) return layer.msg("获取分类失败");
+        if (res.status !== 0) return layer.msg('获取分类失败')
         //调用模板引擎,渲染分类的下拉菜单
-        let htmlStr = template("tpl-edit", res);
+        let htmlStr = template('tpl-edit', res)
         // console.log($("#edit_id"));
-        $("[name=cate_id]").html(htmlStr);
+        $('[name=cate_id]').html(htmlStr)
         // 调用form.render()渲染下拉菜单
-        form.render();
+        form.render()
       },
-    });
+    })
   }
   //初始化封面裁剪区域
   // 1. 初始化图片裁剪器
-  var $image = $("#image");
+  var $image = $('#image')
 
   // 2. 裁剪选项
   var options = {
     aspectRatio: 400 / 280,
-    preview: ".img-preview",
-  };
+    preview: '.img-preview',
+  }
 
   // 3. 初始化裁剪区域
-  $image.cropper(options);
+  $image.cropper(options)
   //为选择封面的按钮，绑定点击事件处理函数
-  $("#btnChooseImage").on("click", function () {
-    $("#coverFile").click();
-  });
+  $('#btnChooseImage').on('click', () => {
+    $('#coverFile').click()
+  })
   //监听 coverFile 的 change 事件，获取用户选择的文件列表
-  $("#coverFile").on("change", function (e) {
+  $('#coverFile').on('change', (e) => {
     //获取到文件的列表数组
-    let files = e.target.files;
+    let files = e.target.files
     if (files.length === 0) return;
     //根据文件，创建一个对应的 URL 地址：
-    var newImgURL = URL.createObjectURL(files[0]);
+    var newImgURL = URL.createObjectURL(files[0])
     //为裁剪区域重新设置图片
     $image
-      .cropper("destroy") // 销毁旧的裁剪区域
-      .attr("src", newImgURL) // 重新设置图片路径
-      .cropper(options); // 重新初始化裁剪区域
-  });
+      .cropper('destroy') // 销毁旧的裁剪区域
+      .attr('src', newImgURL) // 重新设置图片路径
+      .cropper(options) // 重新初始化裁剪区域
+  })
   /**
    * 发布文章的实现步骤
    * 1. 获取表单数据
@@ -91,27 +91,27 @@ $(function () {
    *   (3.根据返回结果，进行跳转
    */
   //定义文章的发布状态
-  let state = "已发布";
+  let state = '已发布'
   //为存为重置按钮绑定点击事件处理函数
-  $("#btnReset").on("click", function () {
-    window.location.reload();
-  });
+  $('#btnReset').on('click', () => {
+    window.location.reload()
+  })
   //为表单绑定submit事件处理函数
-  $("#form-edit").on("submit", function (e) {
+  $('#form-edit').on('submit', (e) => {
     //阻止表单的默认提交行为
-    e.preventDefault();
+    e.preventDefault()
     //基于form表单，快速创建一个formData对象
-    let fd = new FormData($(this)[0]);
+    let fd = new FormData($('#form-edit')[0])
     // console.log($(this)[0]);
     // console.log(fd);
     //将文章的发布状态存到FormData对象中
-    fd.append("state", state);
+    fd.append('state', state);
     // fd.forEach((value, key) => {
     //   console.log(key, value);
     // });
     //将封面裁剪过的图片加入到FormData对象中
     $image
-      .cropper("getCroppedCanvas", {
+      .cropper('getCroppedCanvas', {
         // 创建一个 Canvas 画布
         width: 400,
         height: 280,
@@ -121,20 +121,20 @@ $(function () {
         // 得到文件对象后，进行后续的操作
         // 将文件对象，存储到 FormData 对象实例中
         // console.log(blob);
-        fd.append("cover_img", blob);
+        fd.append('cover_img', blob)
 
         fd.forEach((value, key) => {
-          console.log(key, value);
-        });
+          console.log(key, value)
+        })
         publishArticle(fd);
-      });
-  });
+      })
+  })
   //定义一个发布文章的方法
   function publishArticle(fd) {
     //发送ajax请求，实现文章的发布
     $.ajax({
-      method: "POST",
-      url: "/my/article/edit",
+      method: 'POST',
+      url: '/my/article/edit',
       data: fd,
       //如果向服务器提交的数据是FormData类型，必须设置下面的两个属性
       //告诉$.ajax方法不要解析请求参数
@@ -142,14 +142,14 @@ $(function () {
       //告诉$.ajax方法不要设置请求参数的类型
       contentType: false,
       success: (res) => {
-        console.log(res);
-        if (res.status !== 0) return layer.msg(res.message);
+        console.log(res)
+        if (res.status !== 0) return layer.msg(res.message)
         //提示用户,发布文章成功
-        layer.msg(res.message);
+        layer.msg(res.message)
         //跳转到文章列表页面
-        location.href = "/article/article_list.html";
+        location.href = '/article/article_list.html'
         // initTable();
       },
-    });
+    })
   }
-});
+})
